162e639ea20e18baf6b5562ed1da074095b32511
fernandacoto <fermarioro@gmail.com>
2014-09-17 17:09:39 -0600

    Added styles to back button on activity page, create provinces table and task to fill it up

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,5 @@
 class Activity < ActiveRecord::Base
  belongs_to :category
+ belongs_to :province
  paginates_per 6
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140917222601) do
+ActiveRecord::Schema.define(version: 20140917224359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,10 @@ ActiveRecord::Schema.define(version: 20140917222601) do
   create_table "categories", force: true do |t|
     t.string "name"
     t.string "url"
+  end
+
+  create_table "provinces", force: true do |t|
+    t.string "name"
   end
 
 end
