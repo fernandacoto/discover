21486da946110df4184498ae9e5e9c698cc07ac9
fernandacoto <fermarioro@gmail.com>
2014-10-01 11:45:53 -0600

    added new spaces on activity table into admin section

--- app/admin/activity.rb
+++ app/admin/activity.rb
@@ -1,3 +1,3 @@
 ActiveAdmin.register Activity do
- permit_params :name, :description, :img_url, :category_id
+ permit_params :name, :description, :img_url, :category_id,:phone, :attractions, :website, :entrance_price, :province_id
 end
